0e62f3f99041cd5259be65935a66467224d6da0f
Christina Milon <celestianc.73@gmail.com>
2024-02-21 05:49:02 +0000

    added new doctstring to add_num

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,5 +1,6 @@
 def add_numbers(a,b,c):
     """ Return the sum of a,b and c"""
+    """ More docstrings. should be visible only to feat branch"""
     return a + b + c
 
 def subtract_numbers(a,b):
