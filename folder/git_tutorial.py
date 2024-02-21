21ae1050717e3f503fa0af143bcd7957a75bda99
Christina Milon <celestianc.73@gmail.com>
2024-02-21 05:54:35 +0000

    1

--- folder/git_tutorial.py
+++ folder/git_tutorial.py
@@ -1,5 +1,10 @@
 def add_numbers(a,b,c):
     return a + b + c
 
+#1. extend to allow a no.of parameters
+#2.investigate and resolve security exploit of add_nums
+#3.make implementation more performant add_num()
+#4.add documentation for add_num()
+
 def subtract_numbers(a,b):
     return a - b 
